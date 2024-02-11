ed82d16f0bcec0f5d9018ac831e3151e9d72fe6b
Akash <akash.enlighten@gmail.com>
2024-02-12 00:59:58 +0530

    AppNavigator

--- App.js
+++ App.js
@@ -1,36 +1,8 @@
 import React from 'react';
 
-import { NavigationContainer } from '@react-navigation/native';
-
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
 import { useFonts, RobotoSlab_400Regular, RobotoSlab_500Medium, RobotoSlab_600SemiBold, RobotoSlab_900Black } from '@expo-google-fonts/roboto-slab';
 
-import Login from './src/screens/Auth/Login';
-import ForgotPassword from './src/screens/Auth/ForgotPassword';
-import Register from './src/screens/Auth/Register';
-
-import HomeScreen from './src/screens/HomeScreen'
-
-const AuthStack = createNativeStackNavigator();
-const Auth = () => {
-    return (
-        <AuthStack.Navigator initialRouteName="Login" screenOptions={() => ({ headerShown: false })}>
-            <AuthStack.Screen name="Login" component={Login} />
-            <AuthStack.Screen name="Register" component={Register} />
-            <AuthStack.Screen name="ForgotPassword" component={ForgotPassword} />
-        </AuthStack.Navigator>
-    );
-}
-
-const MainStack = createNativeStackNavigator();
-const Main = () => {
-    return (
-        <MainStack.Navigator initialRouteName="Home">
-            <MainStack.Screen name="Home" component={HomeScreen}/>
-        </MainStack.Navigator>
-    );
-}
+import AppNavigator from './src/navigation/AppNavigator';
 
 const App = () =>{
 
@@ -47,16 +19,8 @@ const App = () =>{
         return null;
     }
 
-    const user = true;
-
     return (
-        <NavigationContainer>
-            
-            {user != true && <Auth/> }
-
-            {user == true && <Main />}
-
-        </NavigationContainer>
+        <AppNavigator/>
     );
 }
 
---
+++ src/navigation/AppNavigator.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+import { NavigationContainer } from "@react-navigation/native";
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Login from '../screens/Auth/Login';
+import ForgotPassword from '../screens/Auth/ForgotPassword';
+import Register from '../screens/Auth/Register';
+
+import HomeScreen from '../screens/HomeScreen';
+
+const AuthStack = createNativeStackNavigator();
+const Auth = () => {
+    return (
+        <AuthStack.Navigator initialRouteName="Login" screenOptions={() => ({ headerShown: false })}>
+            <AuthStack.Screen name="Login" component={Login} />
+            <AuthStack.Screen name="Register" component={Register} />
+            <AuthStack.Screen name="ForgotPassword" component={ForgotPassword} />
+        </AuthStack.Navigator>
+    );
+}
+
+const MainStack = createNativeStackNavigator();
+const Main = () => {
+    return (
+        <MainStack.Navigator initialRouteName="Home">
+            <MainStack.Screen name="Home" component={HomeScreen}/>
+        </MainStack.Navigator>
+    );
+}
+
+export default () => {
+    const user = true;
+    return (
+        <NavigationContainer>
+            
+            {user != true && <Auth/> }
+
+            {user == true && <Main />}
+
+        </NavigationContainer>
+    );
+}
