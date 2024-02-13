8de413c0407c2b076404b8ebe7904be4ed83fa01
Akash <akash.enlighten@gmail.com>
2024-02-13 14:03:23 +0530

    expo-contacts

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import * as Contacts from 'expo-contacts';
 
 import { View, Text, StyleSheet } from 'react-native';
 
@@ -6,6 +7,20 @@ import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 const Home = () => {
     const insets = useSafeAreaInsets();
+    useEffect(() => {
+        (async () => {
+          const { status } = await Contacts.requestPermissionsAsync();
+          if (status === 'granted') {
+            const { data } = await Contacts.getContactsAsync({
+              fields: [Contacts.Fields.PhoneNumbers], // Requesting phone numbers
+            });
+    
+            if (data.length > 0) {
+              console.log(JSON.stringify(data, null, 2));
+            }
+          }
+        })();
+      }, []);
     return (
         <View style={[styles.container, {paddingTop: insets.top}]}>
              <Text style={{fontFamily: 'RobotoSlab_400Regular'}}>Home Screen</Text>
