cd8646264cf2d9ea749fb9c1502db9ba8410deba
Akash <akash.enlighten@gmail.com>
2024-02-13 11:52:22 +0530

    Small Changes

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -8,7 +8,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome6  from 'react-native-vector-icons/FontAwesome6';
 
 import { windowHeight } from '../utils/Dimensions';
 
@@ -22,20 +22,19 @@ import Account from '../screens/AccountScreen';
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
-const screenOptionStyle = { headerShown: false };
-
 const iconConfig = {
-  Home: { focused: 'home', unfocused: 'home-outline' },
-  Account: { focused: 'person', unfocused: 'person-outline' },
+  Home: { focused: 'house', unfocused: 'house' },
+  Account: { focused: 'user', unfocused: 'user' },
 };
 
 const tabBarIcon = ({ route, focused, color, size }) => {
   const iconName = focused ? iconConfig[route.name].focused : iconConfig[route.name].unfocused;
   
-  return <Ionicons name={iconName} size={size} color={color} />;
+  return <FontAwesome6 name={iconName} size={size} color={color} solid/>;
 };
+
 const AuthStack = () => (
-  <Stack.Navigator screenOptions={screenOptionStyle}>
+  <Stack.Navigator screenOptions={{ headerShown: false }}>
     <Stack.Screen name="Login" component={Login} />
     <Stack.Screen name="Register" component={Register} />
     <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
@@ -45,20 +44,27 @@ const AuthStack = () => (
 const MainStack = () => (
   <Tab.Navigator
     initialRouteName="Home"
+
     screenOptions={({ route }) => ({
       headerShown: false,
+
       tabBarIcon: ({ focused, color, size }) => tabBarIcon({ route, focused, color, size }),
+      
+      tabBarStyle: { height: windowHeight/15, position: 'relative' },
       tabBarLabelStyle: { fontFamily: 'RobotoSlab_400Regular' },
-      tabBarStyle: { height: windowHeight/15 },
-      tabBarActiveTintColor: 'tomato',
-      tabBarInactiveTintColor: 'gray',
+
+      tabBarActiveTintColor: 'black',
+      tabBarInactiveTintColor: 'grey',
+
+      tabBarActiveBackgroundColor: 'white',
+
       tabBarShowLabel: false,
       tabBarLabelPosition: 'below-icon',
-      tabBarActiveBackgroundColor: 'grey'
+      
     })}
   >
-    <Tab.Screen name="Home" component={Home} />
-    <Tab.Screen name="Account" component={Account} />
+    <Tab.Screen name="Home" component={Home}/>
+    <Tab.Screen name="Account" component={Account}/>
   </Tab.Navigator>
 );
 
@@ -66,7 +72,7 @@ const App = () => {
   const user = true; // This would be dynamic based on authentication status
   return (
     <>
-      <StatusBar style="auto" backgroundColor='#ffffff' animated={true} hidden={false} StatusBarAnimation='fade'/>
+      <StatusBar style="dark" backgroundColor='#ffffff' animated={true} hidden={false} StatusBarAnimation='fade'/>
       <NavigationContainer>
         {user ? <MainStack /> : <AuthStack />}
       </NavigationContainer>
