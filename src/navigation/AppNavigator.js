e32799a5a86f07f2c4b3d2f79395474cb200995f
Akash <akash.enlighten@gmail.com>
2024-02-14 10:18:50 +0530

    Efficient App Navigator

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,15 +1,9 @@
 import React from 'react';
-
 import { StatusBar } from 'expo-status-bar';
-
 import { NavigationContainer } from '@react-navigation/native';
-
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
-import FontAwesome6  from 'react-native-vector-icons/FontAwesome6';
-
+import FontAwesome6 from 'react-native-vector-icons/FontAwesome6';
 import { windowHeight } from '../utils/Dimensions';
 
 // Screens
@@ -22,16 +16,19 @@ import Account from '../screens/AccountScreen';
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
-const iconConfig = {
-  Home: { focused: 'house', unfocused: 'house' },
-  Account: { focused: 'user', unfocused: 'user' },
+const ICON_NAMES = {
+  Home: 'house',
+  Account: 'user',
 };
 
-const tabBarIcon = ({ route, focused, color, size }) => {
-  const iconName = focused ? iconConfig[route.name].focused : iconConfig[route.name].unfocused;
-  
-  return <FontAwesome6 name={iconName} size={size} color={color} solid/>;
-};
+const tabBarIcon = ({ route, color, size }) => (
+  <FontAwesome6 name={ICON_NAMES[route.name]} size={size} color={color} solid />
+);
+
+const tabBarStyle = { height: windowHeight / 15, position: 'relative' };
+const tabBarLabelStyle = { fontFamily: 'RobotoSlab_400Regular' };
+const tabBarActiveTintColor = '#FF5A5F';
+const tabBarInactiveTintColor = '#6D6D64';
 
 const AuthStack = () => (
   <Stack.Navigator screenOptions={{ headerShown: false }}>
@@ -44,40 +41,31 @@ const AuthStack = () => (
 const MainStack = () => (
   <Tab.Navigator
     initialRouteName="Home"
-
     screenOptions={({ route }) => ({
       headerShown: false,
-
-      tabBarIcon: ({ focused, color, size }) => tabBarIcon({ route, focused, color, size }),
-      
-      tabBarStyle: { height: windowHeight/15, position: 'relative' },
-      tabBarLabelStyle: { fontFamily: 'RobotoSlab_400Regular' },
-
-      tabBarActiveTintColor: '#FF5A5F',
-      tabBarInactiveTintColor: '#6D6D64',
-
-      tabBarActiveBackgroundColor: 'white',
-
+      tabBarIcon: props => tabBarIcon({ route, ...props }),
+      tabBarStyle,
+      tabBarLabelStyle,
+      tabBarActiveTintColor,
+      tabBarInactiveTintColor,
       tabBarShowLabel: false,
       tabBarLabelPosition: 'below-icon',
-      
     })}
   >
-    <Tab.Screen name="Home" component={Home}/>
-    <Tab.Screen name="Account" component={Account}/>
+    <Tab.Screen name="Home" component={Home} />
+    <Tab.Screen name="Account" component={Account} />
   </Tab.Navigator>
 );
 
 const App = () => {
-  const user = true; // This would be dynamic based on authentication status
+  const user = true; // Consider replacing this with dynamic auth status
   return (
     <>
-      <StatusBar style="dark" backgroundColor='#ffffff' animated={true} hidden={false} StatusBarAnimation='fade'/>
+      <StatusBar style="dark" backgroundColor='#ffffff' animated hidden={false} />
       <NavigationContainer>
         {user ? <MainStack /> : <AuthStack />}
       </NavigationContainer>
     </>
-    
   );
 };
 
